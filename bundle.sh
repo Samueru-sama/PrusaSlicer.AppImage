a7901d724d25ad9eeda45d2a57c0162e78d028c8
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-29 11:06:33 -0400

    add `gbm`

--- bundle.sh
+++ bundle.sh
@@ -10,7 +10,7 @@ export APPIMAGE_EXTRACT_AND_RUN=1
 
 APPIMAGETOOL="https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|continuous|*$ARCH.AppImage.zsync"
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/tags/v0.4.4/lib4bin"
+LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/tags/v0.6.5/lib4bin"
 
 # Prepare AppDir
 mkdir -p ./"$PACKAGE"/AppDir/shared/lib \
@@ -34,6 +34,7 @@ chmod +x ./lib4bin
 xvfb-run -a -- ./lib4bin -p -v -e -s -k \
   /usr/bin/prusa-slicer \
   /usr/bin/OCCTWrapper.so \
+  /usr/lib/"$ARCH"-linux-gnu/gbm/* \
   /usr/lib/"$ARCH"-linux-gnu/libwebkit2gtk* \
   /usr/lib/"$ARCH"-linux-gnu/gdk-pixbuf-*/*/*/* \
   /usr/lib/"$ARCH"-linux-gnu/gio/modules/* \
